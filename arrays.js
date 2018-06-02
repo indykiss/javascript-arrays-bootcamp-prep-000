63727eb90e94d61d409536fc62df31bee7e5669c
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-06-02 20:43:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element) {
   var array = [];
   var newArray= ["element", ...array]
+  return newArrayl
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
